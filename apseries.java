03b09abbccc016fd17ca17dfddc0f700c67e23c1
ommprasad-programmer <nathommprasad@gmail.com>
2025-12-23 10:01:45 +0530

    added apseries

--- apseries.java
+++ apseries.java
@@ -1,12 +1,17 @@
-package javadsa;
-
 public class apseries {
     public static void main(String[] args) {
-        int i;
-         for(i=99; i>=0 ;  i-=4){
-           
-            System.out.println(i);
-         }
-}
+        int firstTerm = 2;       // The starting number (a)
+        int commonDifference = 3;  // The common difference (d)
+        int numberOfTerms = 5;   // The number of terms to print (n)
+
+        System.out.print("Arithmetic Progression: ");
+
+        for (int i = 0; i < numberOfTerms; i++) {
+            // Calculate the current term using the formula: a + i * d
+            int currentTerm = firstTerm + i * commonDifference;
+            System.out.print(currentTerm + " ");
+        }
+
+        System.out.println(); // Prints a newline character at the end
     }
-   
+}
