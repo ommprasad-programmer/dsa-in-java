86d3e08acb11412178b5a5c8c42733b8ccac5666
ommprasad-programmer <nathommprasad@gmail.com>
2026-01-17 23:19:35 +0530

    hello

---
+++ arrays/second.java
@@ -0,0 +1,18 @@
+package javadsa.arrays;
+
+public class second {
+
+
+   
+
+
+   public static void main(String[] args) {
+    int[] arr = new int[4];
+    arr[0]=51;
+    arr[1]=39;
+    arr[2]=38;
+    arr[3]=59;
+    System.out.println(arr[3]);
+   }    
+
+}
